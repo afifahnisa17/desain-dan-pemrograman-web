dc75f09b5a9944f70e9ba73da6d9682ca2d06181
Afifah Khoirunnisa <afifahnisa041@gmail.com>
2024-09-26 07:34:51 +0700

    update file fungsi.php, fungsi parameter

--- dasarWeb/fungsi.php
+++ dasarWeb/fungsi.php
@@ -1,13 +1,20 @@
 <?php
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Elok<br>";
+function perkenalan($nama, $salam){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya $nama<br>";
     echo "Senang berkenalan dengan Anda<br>";
 
 
 }
 
 //memanggil fungsi yang sudah dibuat
-perkenalan();
-perkenalan();
+perkenalan("Hamdana", "Hallo");
+
+echo "<hr>";
+
+$saya = "Elok";
+$ucapanSalam = "Selamat pagi";
+
+//memanggil lagi tanpa mengisi parameter salam
+perkenalan($saya, $ucapanSalam);
 ?>
