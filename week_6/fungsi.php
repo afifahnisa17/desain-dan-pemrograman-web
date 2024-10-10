360da7bf0194fb11530152e7c3b248ddc1c86678
Afifah Khoirunnisa <afifahnisa041@gmail.com>
2024-10-10 07:59:12 +0700

    add file fungsi.php

---
+++ week_6/fungsi.php
@@ -0,0 +1,11 @@
+<?php
+
+function perkenalan(){
+    echo "Assalamu'alaikum, ";
+    echo "Perkenalkan, nama saya Afifah<br>";
+    echo "Senang bertemu dengan Anda<br>";
+}
+
+perkenalan();
+perkenalan()
+?>
