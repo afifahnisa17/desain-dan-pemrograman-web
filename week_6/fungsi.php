2e8562dcd56279408be56efc01da5206c7a83072
Afifah Khoirunnisa <afifahnisa041@gmail.com>
2024-10-10 20:56:54 +0700

    update file fungsi.php

--- week_6/fungsi.php
+++ week_6/fungsi.php
@@ -5,5 +5,13 @@ function hitungUmur($thn_lahir, $tahun_sekarang){
     return $umur;
 }
 
-echo "Umur saya adalah " . hitungUmur(2005, 2024) . " tahun";
+function perkenalan($nama, $salam="Assalamu'alaikum"){
+    echo $salam . ", ";
+    echo "Perkenalkan, nama saya " . $nama . "<br/";
+
+    echo "Saya berusia " . hitungUmur(2005, 2024) . " tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+perkenalan("Afifah");
 ?>
