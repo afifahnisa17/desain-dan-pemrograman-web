a97f2ab256f2e084a8dfa33a31f86df8c122d5cb
Afifah Khoirunnisa <afifahnisa041@gmail.com>
2024-10-16 21:26:01 +0700

    update file isset.php

--- week_7/isset.php
+++ week_7/isset.php
@@ -1,7 +1,8 @@
 <?php
-if(isset($umur) && $umur >= 18){
-    echo "Anda sudah dewasa.";
+$data = array("nama" => "Jane", "usia" => 25);
+if(isset($data["nama"])){
+    echo "Nama: " . $data["nama"];
 } else {
-    echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan";
+    echo "Variabel 'nama' tidak ditemukan dalam array.";
 }
 ?>
